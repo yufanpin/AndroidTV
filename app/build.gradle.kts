50b6d7de7066d7a62467c09745c8f8d073c8829d
yufanpin <3281107880@qq.com>
2026-05-29 20:43:01 +0800

    修复bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,11 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
 
 dependencies {
